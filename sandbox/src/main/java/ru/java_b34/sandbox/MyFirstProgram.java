ac76d878a379b942713dff9e634094d3cb2a1e39
Harlii <dirol779@rambler.ru>
2022-08-17 23:19:20 +0300

    Задание №2: Вычисление расстояния между двумя точками v.2

--- sandbox/src/main/java/ru/java_b34/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/java_b34/sandbox/MyFirstProgram.java
@@ -5,7 +5,8 @@ public class MyFirstProgram {
 	public static void main(String[] args) {
 		System.out.println("Hello, World!");
 
-		Point p = new Point(1,2,4,6);
-		System.out.println("Расстояние между двумя точками p1(" + p.x1 + "," + p.y1 + ") и p2(" + p.x2 + "," + p.y2 + ") = " + p.distance());
+		Point p1 = new Point(1,2);
+		Point p2 = new Point(4,6);
+		System.out.println("Расстояние между двумя точками p1(" + p1.x + "," + p1.y + ") и p2(" + p2.x + "," + p2.y + ") = " + p1.distance(p2));
 	}
 }
