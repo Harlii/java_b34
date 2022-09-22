16d8ff9eeb88159f08a13afebbc05c5134007870
Harlii <dirol779@rambler.ru>
2022-09-22 09:00:09 +0300

    Загрузка тестовых данных из файла

--- addressbook-web-tests/src/test/java/ru/java_b34/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/java_b34/addressbook/tests/GroupCreationTests.java
@@ -5,6 +5,7 @@ import org.testng.annotations.Test;
 import ru.java_b34.addressbook.model.GroupData;
 import ru.java_b34.addressbook.model.Groups;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -15,11 +16,15 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class GroupCreationTests extends TestBase {
 
   @DataProvider
-  public Iterator<Object[]> validGroups() {
+  public Iterator<Object[]> validGroups() throws IOException {
     List<Object[]> list = new ArrayList<Object[]>();
-    list.add(new Object[] {new GroupData("test 1", "header 1", "footer 1")});
-    list.add(new Object[] {new GroupData("test 2", "header 2", "footer 2")});
-    list.add(new Object[] {new GroupData("test 3", "header 3", "footer 3")});
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.csv")));
+    String line = reader.readLine();
+    while (line != null) {
+      String[] split = line.split(";");
+      list.add(new Object[] {new GroupData(split[0], split[1], split[2])});
+      line = reader.readLine();
+    }
     return list.iterator();
   }
 
