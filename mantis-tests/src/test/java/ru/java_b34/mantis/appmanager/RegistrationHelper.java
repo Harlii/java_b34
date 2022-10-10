7e474495336295fb3b039a4d54b8c34f98511638
Harlii <dirol779@rambler.ru>
2022-10-10 21:52:13 +0300

    8.4. Ленивая инициализация помощников

---
+++ mantis-tests/src/test/java/ru/java_b34/mantis/appmanager/RegistrationHelper.java
@@ -0,0 +1,19 @@
+package ru.java_b34.mantis.appmanager;
+
+import org.openqa.selenium.WebDriver;
+
+public class RegistrationHelper {
+
+  private final ApplicationManager app;
+  private WebDriver wd;
+
+  public RegistrationHelper (ApplicationManager app){
+    this.app = app;
+    wd = app.getDriver(); //ленивая инициализация
+  }
+
+  public void start(String username, String email) {
+    wd.get(app.getProperty("web.baseUrl") + "/signup_page.php");
+  }
+
+}
