ba6eb6e49462987067bc24562e41baa75dc84821
Harlii <dirol779@rambler.ru>
2022-10-11 21:39:09 +0300

    8.6. Получение почты на встроенный в тесты почтовый сервер

---
+++ mantis-tests/src/test/java/ru/java_b34/mantis/model/MailMessage.java
@@ -0,0 +1,12 @@
+package ru.java_b34.mantis.model;
+
+public class MailMessage {
+  public String to;
+  public String text;
+
+  public MailMessage(String to, String text) {
+    this.to = to;
+    this.text = text;
+  }
+
+}
